9b8eb37dfb2f85ee22100ae37254dc5739504a0c
Stranger <hardikp888@gmail.com>
2015-01-11 14:26:19 +0530

    Create Recycler view and card view raw.

---
+++ app/src/main/java/com/example/hardik/knapsack/UI/ExpenseAdapter.java
@@ -0,0 +1,59 @@
+package com.example.hardik.knapsack.UI;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.hardik.knapsack.BL.Expense;
+import com.example.hardik.knapsack.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Stranger on 1/11/2015.
+ */
+public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ExpenseHolder> {
+
+    private ArrayList<Expense> mExpenseList;
+
+    public ExpenseAdapter(ArrayList<Expense> expenseList) {
+        mExpenseList = expenseList;
+    }
+
+
+    @Override
+    public ExpenseHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_raw, parent, false);
+        return new ExpenseHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ExpenseHolder holder, int position) {
+        Expense expense = mExpenseList.get(position);
+
+        holder.mTxtType.setText(expense.getType());
+        holder.mTxtAmount.setText(expense.getDate());
+        holder.mTxtDate.setText(expense.getDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExpenseList.size();
+    }
+
+    public static class ExpenseHolder extends RecyclerView.ViewHolder {
+
+        final TextView mTxtType;
+        final TextView mTxtAmount;
+        final TextView mTxtDate;
+
+        public ExpenseHolder(View itemView) {
+            super(itemView);
+            mTxtType = (TextView) itemView.findViewById(R.id.txt_exp_type);
+            mTxtAmount = (TextView) itemView.findViewById(R.id.txt_exp_amount);
+            mTxtDate = (TextView) itemView.findViewById(R.id.txt_exp_date);
+        }
+    }
+}
--- app/src/main/java/com/example/hardik/knapsack/UI/ViewExpenseFragment.java
+++ app/src/main/java/com/example/hardik/knapsack/UI/ViewExpenseFragment.java
@@ -3,21 +3,56 @@ package com.example.hardik.knapsack.UI;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import com.example.hardik.knapsack.BL.Expense;
+import com.example.hardik.knapsack.DataBase.DataBaseAdapter;
 import com.example.hardik.knapsack.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by Stranger on 1/8/2015.
  */
 public class ViewExpenseFragment extends Fragment {
 
+    private RecyclerView mRecyclersViewExpense;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_view, container, false);
 
+        mRecyclersViewExpense = (RecyclerView) view.findViewById(R.id.recycler_expense);
+        ImageButton btnAdd = (ImageButton) view.findViewById(R.id.imgBtn_add);
+        ViewCompat.setElevation(btnAdd, 6);
+        btnAdd.setOnClickListener(mAddClickListener);
+
+        DataBaseAdapter dataBaseAdapter = new DataBaseAdapter(getActivity());
+        ArrayList<Expense> list = dataBaseAdapter.getAllExpense();
+        ExpenseAdapter adapter = new ExpenseAdapter(list);
+        mRecyclersViewExpense.setAdapter(adapter);
+        mRecyclersViewExpense.setLayoutManager(new LinearLayoutManager(getActivity()));
+
         return view;
     }
+
+
+    private final View.OnClickListener mAddClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            InsertExpenseFragment insertExpenseFragment = new InsertExpenseFragment();
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(android.R.id.tabcontent, insertExpenseFragment);
+            fragmentTransaction.commit();
+
+        }
+    };
 }
