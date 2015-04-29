5af1a9f65b8ea039aefd5d21e610a252f283ac8b
Stranger <hardikp888@gmail.com>
2015-04-29 20:09:13 +0530

    Add Capture image functionality

--- app/src/main/java/com/example/hardik/knapsack/UI/InsertExpenseFragment.java
+++ app/src/main/java/com/example/hardik/knapsack/UI/InsertExpenseFragment.java
@@ -1,12 +1,19 @@
 package com.example.hardik.knapsack.UI;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,44 +22,57 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.hardik.knapsack.BL.Expense;
 import com.example.hardik.knapsack.BL.Global;
 import com.example.hardik.knapsack.DataBase.DataBaseAdapter;
 import com.example.hardik.knapsack.R;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
-/**
- * Created by Stranger on 1/8/2015.
- */
 public class InsertExpenseFragment extends Fragment {
 
-    private final View.OnClickListener mInsertClick = new View.OnClickListener() {
+    private final static String TAG = "InsertExpenseFragment";
+    private static final int CAPTURE_PHOTO = 100;
+    Dialog mDialogList;
+    DatePickerDialog.OnDateSetListener mDateChangedListener = new DatePickerDialog.OnDateSetListener() {
         @Override
-        public void onClick(View v) {
-            Expense expense = new Expense();
-            expense.setType(mEdTxtType.getText().toString().trim());
-            expense.setDate(mEdTxtDate.getText().toString().trim());
-            expense.setDescription(mEdTxtDes.getText().toString().trim());
-            expense.setAmount(Integer.parseInt(mEdTxtAmount.getText().toString().trim()));
-
-            DataBaseAdapter dataBaseAdapter = new DataBaseAdapter(getActivity());
-            dataBaseAdapter.insetExpense(expense);
-
-            Intent intent = new Intent(getActivity(), MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            getActivity().finish();
-            startActivity(intent);
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
         }
     };
-    private final View.OnFocusChangeListener mDateFocusChange = new View.OnFocusChangeListener() {
+    private String mPhotoFile = "";
+    private Uri mFileUri = null;
+    private final View.OnClickListener mCaptureClickListener = new View.OnClickListener() {
         @Override
-        public void onFocusChange(View v, boolean hasFocus) {
-            if (hasFocus) {
-                showDateDialog();
+        public void onClick(View v) {
+            mFileUri = getFileUri();
+            final Intent cameraIntent = new Intent();
+            cameraIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, mFileUri);
+            startActivityForResult(cameraIntent, CAPTURE_PHOTO);
+        }
+    };
+    private EditText mEdTxtType;
+    private final AdapterView.OnItemClickListener mExpListItemClick = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String expenseType = (String) parent.getItemAtPosition(position);
+            if (expenseType != null && expenseType.length() > 0) {
+                mEdTxtType.setText(expenseType);
+            }
+
+            if (mDialogList != null) {
+                mDialogList.dismiss();
+                mDialogList.cancel();
             }
         }
     };
@@ -70,30 +90,42 @@ public class InsertExpenseFragment extends Fragment {
             showListDialog();
         }
     };
-    Dialog mDialogList;
-    private final AdapterView.OnItemClickListener mExpListItemClick = new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            String expenseType = (String) parent.getItemAtPosition(position);
-            if (expenseType != null && expenseType.length() > 0) {
-                mEdTxtType.setText(expenseType);
-            }
-
-            if (mDialogList != null) {
-                mDialogList.dismiss();
-                mDialogList.cancel();
-            }
-        }
-    };
-    DatePickerDialog.OnDateSetListener mDateChangedListener = new DatePickerDialog.OnDateSetListener() {
-        @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-        }
-    };
-    private EditText mEdTxtType;
     private EditText mEdTxtAmount;
     private EditText mEdTxtDes;
     private EditText mEdTxtDate;
+    private final View.OnFocusChangeListener mDateFocusChange = new View.OnFocusChangeListener() {
+        @Override
+        public void onFocusChange(View v, boolean hasFocus) {
+            if (hasFocus) {
+                showDateDialog();
+            }
+        }
+    };
+    private EditText mEdTxtPhoto;
+    private final View.OnClickListener mInsertClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            if (validateData()) {
+                Expense expense = new Expense();
+                expense.setType(mEdTxtType.getText().toString().trim());
+                expense.setDate(mEdTxtDate.getText().toString().trim());
+                expense.setDescription(mEdTxtDes.getText().toString().trim());
+                expense.setAmount(Integer.parseInt(mEdTxtAmount.getText().toString().trim()));
+
+                DataBaseAdapter dataBaseAdapter = new DataBaseAdapter(getActivity());
+                dataBaseAdapter.insetExpense(expense);
+
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                getActivity().finish();
+                startActivity(intent);
+            } else {
+                Log.d(TAG, "Invalid Data");
+            }
+        }
+    };
+    private ImageView mImgPhoto;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -102,9 +134,13 @@ public class InsertExpenseFragment extends Fragment {
         mEdTxtAmount = (EditText) view.findViewById(R.id.edtxt_amount);
         mEdTxtDes = (EditText) view.findViewById(R.id.edtxt_description);
         mEdTxtDate = (EditText) view.findViewById(R.id.edtxt_date);
+        mEdTxtPhoto = (EditText) view.findViewById(R.id.edTxt_photo);
 
         Button btnInsert = (Button) view.findViewById(R.id.btn_insert_expense);
         btnInsert.setOnClickListener(mInsertClick);
+
+        ImageButton btnPhoto = (ImageButton) view.findViewById(R.id.btn_take_photo);
+        btnPhoto.setOnClickListener(mCaptureClickListener);
 
         mEdTxtDate.setOnFocusChangeListener(mDateFocusChange);
         mEdTxtDate.setOnClickListener(new View.OnClickListener() {
@@ -118,6 +154,28 @@ public class InsertExpenseFragment extends Fragment {
         mEdTxtType.setOnClickListener(mExpTypeOnclick);
 
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_PHOTO) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                mImgPhoto.setVisibility(View.VISIBLE);
+
+                try {
+                    final Bitmap bitmap = BitmapFactory.decodeFile(mFileUri
+                            .getPath());
+
+                    mImgPhoto.setImageBitmap(bitmap);
+
+                } catch (final Exception e) {
+                    Log.d("MainActitivty", "Error in set Image ");
+                }
+            }
+            mEdTxtPhoto.setText(mPhotoFile);
+        }
     }
 
     private void showDateDialog() {
@@ -165,4 +223,77 @@ public class InsertExpenseFragment extends Fragment {
 
         mDialogList.show();
     }
+
+    private Uri getFileUri() {
+
+        final File photoFileDir = new File(
+                Environment.getExternalStorageDirectory() + "/Expense");
+
+        if (!photoFileDir.exists()) {
+            if (!photoFileDir.mkdir()) {
+                Toast.makeText(getActivity(), "Error in cerate directory",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+
+        final String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+
+        final File photoFile = new File(photoFileDir.getPath() + File.separator
+                + "IMG_EXP_" + timeStamp + ".jpg");
+        mPhotoFile = photoFile.getAbsolutePath();
+
+        return Uri.fromFile(photoFile);
+    }
+
+    private boolean validateData() {
+        boolean result = false;
+
+        if (mEdTxtType.getText().toString().length() <= 0) {
+            mEdTxtType.setError(getString(R.string.expense_type));
+            result = false;
+        } else {
+            mEdTxtType.setError(null);
+        }
+
+        if (mEdTxtDate.getText().toString().length() <= 0) {
+            mEdTxtDate.setError(getString(R.string.expense_date));
+            result = false;
+        } else {
+            mEdTxtDate.setError(null);
+        }
+
+        if (mEdTxtDes.getText().toString().length() <= 0) {
+            mEdTxtDes.setError(getString(R.string.expense_des));
+            result = false;
+        } else {
+            mEdTxtDes.setError(null);
+        }
+
+        if (mEdTxtAmount.getText().toString().length() <= 0) {
+            mEdTxtAmount.setError(getString(R.string.expense_amount));
+            result = false;
+        } else {
+            mEdTxtAmount.setError(null);
+        }
+
+        if (mEdTxtPhoto.getText().toString().length() <= 0) {
+            mEdTxtPhoto.setText("N/A");
+        }
+
+
+        return result;
+    }
+
+    /*@Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable("file_uri", mFileUri);
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        mFileUri = savedInstanceState.getParcelable("file_uri");
+    }*/
 }
